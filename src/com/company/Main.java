17c34d49dffa00eeb6e301e75b1b0ed73e5d8f69
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-03-16 19:49:00 +0330

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String [] args){
+        System.out.println("Hello git");
+        System.out.println("My name is: <your name>");
+        System.out.println("My student ID is <your id>");
+    }
+}
