d5507775eaa8efd43714f3477bf26b7f2bb26102
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-03-16 19:52:47 +0330

    name entered

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String [] args){
         System.out.println("Hello git");
-        System.out.println("My name is: <your name>");
+        System.out.println("My name is: <seyed mostafa atyabi>");
         System.out.println("My student ID is <your id>");
     }
 }
